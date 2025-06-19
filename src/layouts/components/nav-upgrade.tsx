a70e2b212d70e6bbb34152a94a8e5f000906edfa
moeez23 <136589035+moeez23@users.noreply.github.com>
2025-06-19 13:13:49 +0500

    updated

--- src/layouts/components/nav-upgrade.tsx
+++ src/layouts/components/nav-upgrade.tsx
@@ -55,7 +55,7 @@ export function NavUpgrade({ sx, ...other }: StackProps) {
         // href="https://material-ui.com/store/items/minimal-dashboard/"
         // target="_blank"
         // variant="contained"
-        onclick="openChatWithExternalBtn()"
+        onClick= {openChatWithExternalBtn}
         color="inherit"
       >
         Upgrade to Pro
